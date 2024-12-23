58c6da7d63b35ef979f984b92bad8690a4330137
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2024-12-23 10:12:25 +0200

    Moved unused keybinds to another file, effectively disabling them.

---
+++ nvim/lua/core/non-active-keymaps.lua
@@ -0,0 +1,40 @@
+-- For conciseness
+local opts = { noremap = true, silent = true }
+
+-- TODO: Understand how to use diagnostics.
+--
+-- Toggle diagnostics
+local diagnostics_active = true
+
+vim.keymap.set('n', '<leader>do', function()
+  diagnostics_active = not diagnostics_active
+
+  if diagnostics_active then
+    vim.diagnostic.enable(0)
+  else
+    vim.diagnostic.disable(0)
+  end
+end)
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- TODO: Activate when I have some autocommands
+--
+-- save file without triggering autocommands
+vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
+
+-- TODO: Decide on whether the below is a good thing.
+--
+-- Allow moving the cursor through wrapped lines with j, k. I'm undecided on whether this is a good thing. It might be worth activitating only for certain filetypes.
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Tabs
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+vim.keymap.set('n', '<leader>tN', ':tabp<CR>', opts) --  go to previous tab
