bd9a1ddca5658256e62deaf380d84391af4139a5
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2024-12-23 11:22:43 +0200

    Added PEP 8 indentation to Python files

---
+++ nvim/after/ftplugin/python.lua
@@ -0,0 +1,9 @@
+local set = vim.opt_local
+-- Add the proper PEP 8 indentation
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+set.textwidth = 79
+set.expandtab = true
+set.autoindent = true
+set.fileformat = 'unix'
