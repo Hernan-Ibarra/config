96004720415da8fda4c8f7cf4b415bba6915377b
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2024-12-23 12:02:34 +0200

    Restructured the treesitter config to follow the recommended one

--- nvim/lua/global-plugins/treesitter.lua
+++ nvim/lua/global-plugins/treesitter.lua
@@ -1,43 +1,42 @@
+local langs = {
+  'lua',
+  'python',
+  'javascript',
+  'typescript',
+  'vimdoc',
+  'vim',
+  'regex',
+  'toml',
+  'json',
+  'java',
+  'gitignore',
+  'markdown',
+  'markdown_inline',
+  'bash',
+  'tsx',
+  'css',
+  'html',
+}
+
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = {
-      'lua',
-      'python',
-      'javascript',
-      'typescript',
-      'vimdoc',
-      'vim',
-      'regex',
-      'toml',
-      'json',
-      'java',
-      'gitignore',
-      'markdown',
-      'markdown_inline',
-      'bash',
-      'tsx',
-      'css',
-      'html',
-    },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  config = function()
+    local configs = require 'nvim-treesitter.configs'
+    configs.setup {
+      ensure_installed = langs,
+
+      -- Install parsers synchronously when set to false (only applied to `ensure_installed`)
+      sync_install = false,
+
+      -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is the name of the parser) list of language that will be disabled
+      highlight = {
+        enable = true,
+        disable = {}, -- List of languages (or rather parsers) for which highlight is disabled
+      },
+      indent = { enable = true },
+    }
+  end,
 }
