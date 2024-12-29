23685a78dbf3a6097c078d4745af79f515332d41
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2024-12-29 16:20:43 +0000

    Add comments explaining the smear plugin

--- nvim/lua/global-plugins/smear.lua
+++ nvim/lua/global-plugins/smear.lua
@@ -1,6 +1,7 @@
+-- Cursor has smear effect, makes it easier for people to see what is happening in vim during pair-programming sessions.
 return {
   'sphamba/smear-cursor.nvim',
-  enabled = false,
+  enabled = false, -- Disable by default, enable during pair-programming
   opts = {
     cursor_color = '#ff8800',
     stiffness = 0.3,
