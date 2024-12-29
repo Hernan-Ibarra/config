56dc4a01fc37f107874da08be4e4361ad503fd03
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2024-12-29 11:17:06 +0000

    Added mapppings for diagnostics and for making and loading sessions

--- nvim/lua/core/non-active-keymaps.lua
+++ nvim/lua/core/non-active-keymaps.lua
@@ -1,27 +1,6 @@
 -- For conciseness
 local opts = { noremap = true, silent = true }
 
--- TODO: Understand how to use diagnostics.
---
--- Toggle diagnostics
-local diagnostics_active = true
-
-vim.keymap.set('n', '<leader>do', function()
-  diagnostics_active = not diagnostics_active
-
-  if diagnostics_active then
-    vim.diagnostic.enable(0)
-  else
-    vim.diagnostic.disable(0)
-  end
-end)
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
 -- TODO: Activate when I have some autocommands
 --
 -- save file without triggering autocommands
@@ -34,7 +13,7 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Tabs
-vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts)   -- open new tab
 vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
-vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
-vim.keymap.set('n', '<leader>tN', ':tabp<CR>', opts) --  go to previous tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts)     --  go to next tab
+vim.keymap.set('n', '<leader>tN', ':tabp<CR>', opts)     --  go to previous tab
