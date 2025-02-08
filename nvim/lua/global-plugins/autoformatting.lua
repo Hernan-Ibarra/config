8d1f347928a3ca7c765b6085681f917a70621579
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-08 19:18:15 +0000

    Switched autoformatter to conform.nvim

--- nvim/lua/global-plugins/autoformatting.lua
+++ nvim/lua/global-plugins/autoformatting.lua
@@ -1,56 +1,47 @@
--- Format on save and linters
-
-return {
-  'nvimtools/none-ls.nvim',
-  dependencies = {
-    'nvimtools/none-ls-extras.nvim',
-    'jayp0521/mason-null-ls.nvim',
-  },
-  config = function()
-    local null_ls = require 'null-ls'
-    local formatting = null_ls.builtins.formatting -- to setup formatters
-    -- local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-
-    -- list of formatters & linters for mason to install
-    require('mason-null-ls').setup {
-      ensure_installed = {
-        'prettier',           -- ts/js formatter
-        'stylua',             -- lua formatter
-        'eslint_d',           -- ts/js linter
-        'shfmt',              -- shell formatter
-        'ruff',               -- python formatter
-        'google_java_format', -- java formatter
-      },
-      -- auto-install configured formatters & linters (with null-ls)
-      automatic_installation = true,
-    }
-
-    local sources = {
-      formatting.prettier.with { filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'css', 'html', 'json', 'markdown' } },
-      formatting.stylua,
-      formatting.shfmt.with { args = { '-i', '4' } },
-      require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
-      require 'none-ls.formatting.ruff_format',
-      formatting.google_java_format,
-    }
-
-    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-    null_ls.setup {
-      -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
-      sources = sources,
-      -- you can reuse a shared lspconfig on_attach callback here
-      on_attach = function(client, bufnr)
-        if client.supports_method 'textDocument/formatting' then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format { async = false }
-            end,
-          })
-        end
+return { -- Autoformat
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
       end,
-    }
-  end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't have a well standardized coding style. You can add additional languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      local lsp_format_opt
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        lsp_format_opt = 'never'
+      else
+        lsp_format_opt = 'fallback'
+      end
+      return {
+        timeout_ms = 500,
+        lsp_format = lsp_format_opt,
+      }
+    end,
+    formatters_by_ft = {
+      css = { 'prettierd' },
+      html = { 'prettierd' },
+      java = { 'google-java-format' },
+      javascript = { 'prettierd' },
+      javascriptreact = { 'prettierd' },
+      json = { 'prettierd' },
+      lua = { 'stylua' },
+      markdown = { 'prettierd' },
+      python = { 'isort', 'black' },
+      sh = { 'shfmt' },
+      typescript = { 'prettierd' },
+      typescriptreact = { 'prettierd' },
+      --         'eslint_d',           -- ts/js linter
+    },
+  },
 }
