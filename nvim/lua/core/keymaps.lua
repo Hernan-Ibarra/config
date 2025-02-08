0286ce69176cdded817fe287329ab12a60e870ee
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-08 07:22:34 +0000

    Removed the vim-tmux-navigator plugin together with related keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -25,24 +25,6 @@ vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
 vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
 vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
 
--- Buffers
-vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
-vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
-vim.keymap.set('n', '<leader>c', ':bdelete!<CR>', opts)   -- close buffer
-vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
-
--- Window management
-vim.keymap.set('n', '<leader>v', '<C-w>v', opts)      -- split window vertically
-vim.keymap.set('n', '<leader>h', '<C-w>s', opts)      -- split window horizontally
-vim.keymap.set('n', '<leader>=', '<C-w>=', opts)      -- make split windows equal width & height
-vim.keymap.set('n', '<leader>sc', ':close<CR>', opts) -- close current split window
-
--- Navigate between splits
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
-
 -- Toggle line wrapping
 vim.keymap.set('n', '<leader>tw', '<cmd>set wrap!<CR>', opts)
 
