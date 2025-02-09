6d763f169b26c8934a98f9ad11ce4f7d37967a7b
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-09 17:40:24 +0000

    Added linter support to Neovim!

--- nvim/lua/global-plugins/lsp.lua
+++ nvim/lua/global-plugins/lsp.lua
@@ -144,6 +144,13 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'eslint_d',
+      'pylint',
+      'prettierd',
+      'google-java-format',
+      'isort',
+      'black',
+      'shfmt',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
