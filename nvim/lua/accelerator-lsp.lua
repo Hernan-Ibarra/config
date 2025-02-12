952c294eb1175f270688dd1707a86113295e790b
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-12 17:23:48 +0000

    Corrected the configuration, now it is working!

--- nvim/lua/accelerator-lsp.lua
+++ nvim/lua/accelerator-lsp.lua
@@ -1,19 +1,18 @@
-local command = 'npm run --silent --prefix /Users/hernan.ibarramejia87/code/accelerator-lsp start'
-
-local client = vim.lsp.start_client {
-  name = 'accelerator-lsp',
-  cmd = { command },
-}
-
-if not client then
-  vim.notify 'there is a problem connecting to the language server'
-  return
-end
-
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
   pattern = { '*.accelerator' },
   callback = function()
+    -- NOTE: Change this to the path to the respository on your machine
+    local path = '/Users/hernan.ibarramejia87/code/accelerator-lsp'
+    local command = { 'npm', 'run', '--silent', '--prefix', path, 'start' }
+
+    local client = vim.lsp.start { name = 'accelerator-lsp', cmd = command }
+
+    if not client then
+      vim.notify 'there is a problem connecting to the accelerator language server'
+      return
+    end
+
     vim.lsp.buf_attach_client(0, client)
-    print 'accelerator-lsp connected!'
+    print 'accelerator language server connected!'
   end,
 })
