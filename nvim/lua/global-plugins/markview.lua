1397207aac53c2d8d5561314d7135758e976c432
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-14 00:38:47 +0000

    Made it so that markview always renders if the file is readonly

--- nvim/lua/global-plugins/markview.lua
+++ nvim/lua/global-plugins/markview.lua
@@ -1,9 +1,63 @@
 return {
   'OXY2DEV/markview.nvim',
   lazy = false,
-  opts = {
-    preview = {
-      icon_provider = 'devicons', -- "mini" or "devicons"
-    },
-  },
+  config = function()
+    local markview = require 'markview'
+
+    local config = { preview = { icon_provider = 'devicons' } }
+    markview.setup(config)
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MarkviewAttach',
+      callback = function(event)
+        --- This will have all the data you need.
+        local data = event.data
+        local isReadOnly = vim.bo[data.buffer].readonly
+
+        if isReadOnly then
+          config.preview.modes = {
+            'n',
+            'no',
+            'nov',
+            'noV',
+            'noCTRL-V',
+            'niI',
+            'niR',
+            'niV',
+            'nt',
+            'ntT',
+            'v',
+            'vs',
+            'V',
+            'Vs',
+            'CTRL-V',
+            'CTRL-Vs',
+            's',
+            'S',
+            'CTRL-S',
+            'i',
+            'ic',
+            'ix',
+            'R',
+            'Rc',
+            'Rx',
+            'Rv',
+            'Rvc',
+            'Rvx',
+            'c',
+            'cr',
+            'cv',
+            'cvr',
+            'r',
+            'rm',
+            'r',
+            '!',
+            't',
+          }
+
+          markview.setup(config)
+        end
+      end,
+    })
+  end,
 }
