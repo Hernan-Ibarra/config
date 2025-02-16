7aeefbdb66fe3533faa8eac56a6d6b5b5725017c
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-16 23:55:51 +0000

    Fixed lua lsp

--- nvim/lua/global-plugins/lsp.lua
+++ nvim/lua/global-plugins/lsp.lua
@@ -89,23 +89,35 @@ return {
       html = { filetypes = { 'html', 'twig', 'hbs' } },
       gopls = {},
       lua_ls = {
-        settings = {
-          Lua = {
-            runtime = { version = 'LuaJIT' },
+        on_init = function(client)
+          if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if path ~= vim.fn.stdpath 'config' and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
+              return
+            end
+          end
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT',
+            },
+            -- Make the server aware of Neovim runtime files
             workspace = {
               checkThirdParty = false,
-              -- Tells lua_ls where to find all the Lua files that you have loaded for your neovim configuration.
               library = {
-                '${3rd}/luv/library',
-                unpack(vim.api.nvim_get_runtime_file('', true)),
+                vim.env.VIMRUNTIME,
+                -- Depending on the usage, you might want to add additional paths here.
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
               },
+              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+              -- library = vim.api.nvim_get_runtime_file("", true)
             },
-            completion = {
-              callSnippet = 'Replace',
-            },
-            telemetry = { enable = false },
-            diagnostics = { disable = { 'missing-fields' } },
-          },
+          })
+        end,
+        settings = {
+          Lua = {},
         },
       },
       tailwindcss = { filetypes = { 'css' } },
@@ -173,3 +185,6 @@ return {
     }
   end,
 }
+--
+-- require'lspconfig'.lua_ls.setup {
+-- }
