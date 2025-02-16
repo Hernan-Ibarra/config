3d001504ebca80a7b6c259317646b43238b3720d
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-16 23:46:13 +0000

    Added treesitter highlighting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,12 @@
 -- NOTE: Keymaps and options are best loaded before lazy.
 require 'core.options'
 require 'core.keymaps'
+
+vim.filetype.add {
+  extension = {
+    accelerator = 'acceleratorscript',
+  },
+}
+
 require 'config.lazy'
 require 'accelerator-lsp'
