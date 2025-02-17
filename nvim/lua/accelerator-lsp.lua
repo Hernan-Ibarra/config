1c438194d96c69119da2a2722472553baea4f549
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-17 00:03:58 +0000

    Added some comments

--- nvim/lua/accelerator-lsp.lua
+++ nvim/lua/accelerator-lsp.lua
@@ -1,12 +1,11 @@
-local notify = require 'notify'
-
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'acceleratorscript',
   callback = function()
     local path = '/Users/hernan.ibarramejia87/code/accelerator-lsp'
     local command = { 'npm', 'run', '--silent', '--prefix', path, 'start' }
-
     local server = vim.lsp.start { name = 'accelerator-lsp', cmd = command }
+
+    local notify = require 'notify'
 
     if not server then
       notify('Could not connect to accelerator language server.', 'error', {
