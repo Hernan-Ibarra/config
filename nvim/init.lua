68786fab419c143ebc80c710e19d5c7ee4995282
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-21 13:25:39 +0000

    Fixed subtle bug with lsp setup; now as filetype plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,6 @@
--- INFO: This matches *.accelerator files and sets filetype to 'acceleratorscript'.
-vim.filetype.add { extension = { accelerator = 'acceleratorscript' } }
-
 require 'core.options'
 require 'core.keymaps'
 require 'config.lazy'
 
-require 'accelerator-lsp' -- INFO: Sets up accelerator-lsp
+-- INFO: This matches *.accelerator files and sets filetype to 'acceleratorscript'.
+vim.filetype.add { extension = { accelerator = 'acceleratorscript' } }
