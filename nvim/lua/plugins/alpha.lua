51878f289fc62016e421b31637db75bf3c090105
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-22 23:18:13 +0000

    Improved nvim start screen

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,24 +1,36 @@
 return {
   'goolord/alpha-nvim',
   dependencies = {
-    'nvim-tree/nvim-web-devicons',
+    { 'echasnovski/mini.icons', version = '*' },
   },
 
   config = function()
     local alpha = require 'alpha'
-    local dashboard = require 'alpha.themes.startify'
+    local dashboard = require 'alpha.themes.dashboard'
 
     dashboard.section.header.val = {
-      [[                                                    ]],
-      [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
-      [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
-      [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
-      [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
-      [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
-      [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
-      [[                                                    ]],
+      '                                                     ',
+      '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+      '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+      '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+      '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+      '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+      '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+      '                                                     ',
     }
 
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
+      dashboard.button('f', '󰈞  > Find file', ':Telescope find_files<CR>'),
+      dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
+      dashboard.button('s', '  > Settings', ':e ~/.config/nvim | :cd ~/.config/nvim<CR>'),
+      dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
+    }
+
+    dashboard.section.footer.val = 'Talk is cheap. Show me the code.'
+
+    -- Send config to alpha
     alpha.setup(dashboard.opts)
   end,
 }
