b4f0ce5158606c0b7665891f10869297cae447e3
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-24 15:05:16 +0000

    Changed the spelling of neovim in the welcome page

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -24,7 +24,7 @@ return {
       dashboard.button('f', '󰈞  > Find file', ':Telescope find_files<CR>'),
       dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
       dashboard.button('s', '  > Settings', ':e ~/.config/nvim | :cd ~/.config/nvim | :Telescope find_files<CR>'),
-      dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
+      dashboard.button('q', '  > Quit NEOVIM', ':qa<CR>'),
     }
 
     local quotes = {
