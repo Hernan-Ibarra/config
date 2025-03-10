f66d8f2359d94648637f60876c079f7b809bf624
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-03-10 09:22:14 +0000

    Modified DelThis command to cover more edge cases

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -12,30 +12,52 @@ vim.api.nvim_create_user_command('Squit', save_and_quit, {
   complete = 'file',
 })
 
-local delete_current_file = function()
-  local arglist_length = vim.fn.argc()
-  local arglist_index = vim.fn.argidx()
-
+local delete_current_file_and_buffer = function(opts)
   local buf = vim.api.nvim_get_current_buf()
-  local full_path = vim.api.nvim_buf_get_name(buf)
-  vim.fn.delete(full_path)
+  local buf_name = vim.api.nvim_buf_get_name(buf)
 
-  vim.cmd.argdelete '%'
-  vim.api.nvim_buf_delete(buf, { force = true })
-
-  if arglist_length == 1 then
+  if (not buf_name) or buf_name == '' then
+    vim.notify('Buffer not associated to a file', vim.log.levels.ERROR)
     return
   end
 
-  if arglist_index == arglist_length - 1 then
-    vim.cmd.argument(1)
+  if (not opts.bang) and vim.api.nvim_get_option_value('confirm', {}) then
+    local message = 'Delete file ' .. vim.fn.fnamemodify(buf_name, ':t') .. '?\nBuffer will be deleted too!'
+    local choice = vim.fn.confirm(message, '&Yes\n&No', 0, 'Question')
+    if choice ~= 1 then
+      return
+    end
+  end
+
+  local delete_success = (vim.fn.delete(buf_name) == 0)
+
+  if not delete_success then
+    vim.notify('Deletion of file' .. buf_name .. ' was unsuccessful.\nAborting...', vim.log.levels.ERROR)
+    return
+  end
+
+  local arglist_length = vim.fn.argc()
+  local arglist_index = vim.fn.argidx()
+
+  local argdelete_success, _ = pcall(function()
+    vim.cmd.argdelete '%'
+  end)
+
+  vim.api.nvim_buf_delete(buf, { force = true })
+
+  if (not argdelete_success) or arglist_length <= 1 then
+    return
+  end
+
+  if arglist_index >= arglist_length - 1 then
+    vim.cmd.first()
     return
   end
 
   vim.cmd.argument(arglist_index + 1)
 end
 
-vim.api.nvim_create_user_command('Delete', delete_current_file, {
+vim.api.nvim_create_user_command('DelThis', delete_current_file_and_buffer, {
   desc = 'Delete the current file and buffer',
 })
 
