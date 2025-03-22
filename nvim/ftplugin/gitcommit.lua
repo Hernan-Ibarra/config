be017deda14fedb4acaf93590cd99a366a7fa03e
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-03-22 17:31:20 +0000

    Fix bug with gitcommit aucommand

--- nvim/ftplugin/gitcommit.lua
+++ nvim/ftplugin/gitcommit.lua
@@ -5,7 +5,7 @@ if vim.b[buf].did_gitcommit_select then
 end
 
 vim.schedule(function()
-  vim.cmd.normal { args = "gggH", bang = true }
+  vim.cmd.normal { args = { "gggH" }, bang = true }
 end)
 
 vim.b[buf].did_gitcommit_select = true
