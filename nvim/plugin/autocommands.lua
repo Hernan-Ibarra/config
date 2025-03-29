1e18570d2ffc11a0e507698d1c001b094629aae5
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-03-29 11:51:44 +0000

    Simplify config, remove some plugins

--- nvim/plugin/autocommands.lua
+++ nvim/plugin/autocommands.lua
@@ -1,39 +1,9 @@
--- TRESITTER FOLDING
-local treesitter_folding = vim.api.nvim_create_augroup("treesitter_folding", { clear = true })
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = treesitter_folding,
-  callback = function(ev)
-    local buf = ev.buf
-    local parsers = require("nvim-treesitter.parsers")
-    local lang = parsers.get_buf_lang(buf)
-    vim.b[buf].has_treesitter_parser = parsers.has_parser(lang)
+-- START TREESITTER
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    pcall(vim.treesitter.start)
   end,
-  desc = "Checks whether buffer has treesitter parser"
-})
-
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  group = treesitter_folding,
-  callback = function(ev)
-    local has_parser = vim.b[ev.buf].has_treesitter_parser
-    if not has_parser then
-      return
-    end
-
-    local win = vim.api.nvim_get_current_win()
-    local set = vim.wo[win]
-    local fdm = vim.api.nvim_get_option_value('foldmethod', { win = win })
-
-    if fdm == 'manual' or fdm == 'syntax' then
-      if vim.api.nvim_get_option_value('filetype', { buf = ev.buf }) == 'json' then
-        set.foldmethod = "indent"
-        return
-      end
-      set.foldexpr = "nvim_treesitter#foldexpr()"
-      set.foldmethod = "expr"
-    end
-  end,
-  desc = "Sets Tresitter folding if parser is found, defaults to syntax folding otherwise"
+  desc = "Start Treesitter"
 })
 
 -- REMEMBER LAST LOCATION
