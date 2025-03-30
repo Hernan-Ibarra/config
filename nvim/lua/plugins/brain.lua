b1317e136b40c4cb7edad282a204ec0f5159e2a0
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-03-30 23:35:15 +0100

    Updated brain.nvim

--- nvim/lua/plugins/brain.lua
+++ nvim/lua/plugins/brain.lua
@@ -1,8 +1,8 @@
 return {
   'Hernan-Ibarra/brain.nvim',
   config = function()
-    vim.keymap.set('n', '<leader>bl', '<cmd>BrainLog<CR>', { desc = 'BrainLog' })
-    vim.keymap.set('n', '<leader>ba', '<cmd>BrainAdd<CR>', { desc = 'BrainAdd' })
+    require('brain').setup({ brain_directory = vim.fs.joinpath(vim.fn.stdpath('data'), 'brain', 'dump') })
+    vim.keymap.set('n', '<leader>bd', '<cmd>BrainDump<CR>', { desc = 'BrainDump' })
     vim.keymap.set('n', '<leader>bo', '<cmd>BrainOrganize<CR>', { desc = 'BrainOrganize' })
   end,
 }
